0ea28570c635ef93f39d77f41f6f5af9b5d8f8af
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-03-25 11:01:09 +0530

    Add project files.

---
+++ Reverse Prefixs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Prefixs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =10;i>0;i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
